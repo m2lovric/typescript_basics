734abba48b5cadff090f0a3577fd8d27645b2d9e
m2lovric <m2lovric@gmail.com>
2021-08-10 00:20:22 +0200

    Type annotations 2

--- types.ts
+++ types.ts
@@ -28,3 +28,14 @@ let point: {x: number, y: number} = {
   x: 10,
   y: 20
 }
+
+const logNumber: (i : number) => void = (i) => {
+  console.log(i);
+}
+
+//when to use annotations
+// 1. function that returns any type
+const json = '{"x" : 10, "y" : 20}';
+const coordinates: {x: number, y: number} = JSON.parse(json);
+
+console.log(coordinates);
